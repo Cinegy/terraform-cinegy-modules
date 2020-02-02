12d461cf928610bc93d9c6cf3d7c3da4d3b69fa6
Lewis Kirkaldie <lewis@gmail.com>
2020-02-02 13:39:44 +0000

    -reworked vpc for TF12

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,33 +1,47 @@
 output "main_vpc" {
-  value = "${aws_vpc.main.id}"
+  value = aws_vpc.main.id
 }
 
 output "main_vpc_cidr" {
-  value = "${aws_vpc.main.cidr_block}"
+  value = aws_vpc.main.cidr_block
 }
 
 output "public_subnets" {
-  value = "${
-    map(
-      "a", "${aws_subnet.public_a.id}",
-      "b", "${aws_subnet.public_b.id}"
-    )
-  }"
+  value = {
+    "a" = aws_subnet.public_a.id
+    "b" = aws_subnet.public_b.id
+  }
 }
 
 output "private_subnets" {
-  value = "${
-    map(
-      "a", "${aws_subnet.private_a.id}",
-      "b", "${aws_subnet.private_b.id}"
-    )
-  }"
+  value = {
+    "a" = aws_subnet.private_a.id
+    "b" = aws_subnet.private_b.id
+  }
 }
 
 output "remote_access_security_group" {
-  value = "${aws_security_group.remote_access.id}"
+  value = aws_security_group.remote_access.id
 }
 
 output "open_internal_access_security_group" {
-  value = "${aws_security_group.open_internal.id}"
+  value = aws_security_group.open_internal.id
 }
+
+output "lambda_base_iam_arn" {
+  value = aws_iam_role.iam_for_logging.arn
+}
+
+output "lambda_iam_policy_logging_arn" {
+  value = aws_iam_policy.cloudwatch_logging.arn
+}
+
+/*
+output "logging_iam_arn" {
+  value = "${aws_iam_role.iam_for_logging.arn}"
+}
+
+
+output "lambda_iam_policy_logging_arn" {
+  value = "${aws_iam_policy.iam_for_logging.arn}"
+}*/
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,6 +1,10 @@
 # Standard variables for managing state and deployment
-variable "aws_region" {
-  description = "AWS region to launch infrastructure within."
+variable "environment_name" {
+  description = "Name to used to label environment deployment, for example 'dev' or 'test-lk'."
+}
+
+variable "account_id" {
+  description = "AWS account ID, used when constructing ARNs for API Gateway."
 }
 
 variable "state_bucket" {
@@ -9,22 +13,65 @@ variable "state_bucket" {
 
 variable "state_region" {
   description = "Region associated with state bucket."
-  default = "eu-west-1"
+  default     = "eu-west-1"
 }
 
-variable "environment_name" {
-  description = "Name to used to label environment deployment, for example 'dev' or 'test-lk'."
+variable "aws_region" {
+  description = "AWS region to launch infrastructure within."
 }
 
 variable "app_name" {
-  description = "Name for labelling the deployment, for example 'sysadmin' or 'playout'"
+  description = "Name to used to label application deployment, for example 'central' or 'air'."
+}
+
+variable "route53_zone_id" {
+  description = "Zone ID of the route 53 zone used to make entries (e.g. sysadmin DNS entries)"
+  default     = ""
+}
+
+variable "route53_zone_suffix" {
+  description = "Zone DNS suffix for public facing entries"
+}
+
+variable "aws_secrets_generic_account_password_arn" {
+  description = "ARN representing general password secret stored within AWS Secrets Manager"
+}
+
+variable "aws_secrets_domain_admin_password_arn" {
+  description = "ARN representing domain admin password key secret stored within AWS Secrets Manager"
+}
+
+variable "aws_secrets_privatekey_arn" {
+  description = "ARN representing private PEM key secret stored within AWS Secrets Manager"
+}
+
+variable "shared_route53_zone_id" {
+  description = "Zone ID of the default shared route 53 zone used to make helper entries (e.g. sysadmin DNS entries)"
+  default     = ""
+}
+
+variable "dynamodb_table" {
+  description = "DynamoDB table used for controlling terragrunt locks"
+}
+
+variable "shared_route53_zone_suffix" {
+  description = "Zone DNS suffix for helper entries (e.g. sysadmin DNS entries)"
+}
+
+variable "domain_name" {
+  description = "Active Directory Domain Name"
+}
+
+variable "domain_default_computer_ou" {
+  description = "Default OU for new computer account creation"
+}
+
+variable "stage" {
+  description = "Deployment stage label, e.g. global or global-temp"
+  default = "global"
 }
 
 # Module specific variables
-variable "aws_secrets_privatekey_arn" {
-  description = "ARN representing private key secret stored within AWS Secrets Manager"
-}
-
 variable "cidr_block" {
   description = "IP range in CIDR format for VPC usage"
 }
@@ -32,12 +79,21 @@ variable "cidr_block" {
 variable "public_a_subnet_cidr_block" {
   description = "IP range in CIDR format for subnet usage"
 }
+
 variable "public_b_subnet_cidr_block" {
   description = "IP range in CIDR format for subnet usage"
 }
+
 variable "private_a_subnet_cidr_block" {
   description = "IP range in CIDR format for subnet usage"
 }
+
 variable "private_b_subnet_cidr_block" {
   description = "IP range in CIDR format for subnet usage"
 }
+
+variable "cinegy_agent_default_manifest_path" {
+  description = "Path to a file containing the defaults to use when creating an Cinegy Agent manifest file"
+  default     = "./conf/defaultproducts.manifest"
+}
+
