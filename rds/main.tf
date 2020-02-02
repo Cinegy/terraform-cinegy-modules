c69e4e0a26805c8e71b1875d87f68e571f362d91
Lewis Kirkaldie <lewis@gmail.com>
2020-02-02 13:44:49 +0000

    -rds to tf12 (lost AD integration for now)

--- rds/main.tf
+++ rds/main.tf
@@ -1,57 +1,48 @@
 terraform {
   # The configuration for this backend will be filled in by Terragrunt
-  backend "s3" {}
-  required_version = ">= 0.11.11"
+  backend "s3" {
+  }
 }
 
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
-    bucket = "${var.state_bucket}"
-    region = "${var.state_region}"
-    key = "${var.environment_name}/vpc/terraform.tfstate"
-  }
-}
-
-data "terraform_remote_state" "directoryservice" {
-  backend = "s3"
-  config {
-    bucket = "${var.state_bucket}"
-    region = "${var.state_region}"
-    key = "${var.environment_name}/directoryservice/terraform.tfstate"
+  config = {
+    bucket = var.global_state_bucket
+    region = var.state_region
+    key    = "${var.environment_name}/vpc/terraform.tfstate"
   }
 }
 
 provider "aws" {
-  region     = "${var.aws_region}"
-  version = "~> 2.3"
+  region  = var.aws_region
+  version = "~> 2.9"
 }
 
 data "aws_subnet_ids" "filtered_subnets" {
-  vpc_id = "${data.terraform_remote_state.vpc.main_vpc}"
+  vpc_id = data.terraform_remote_state.vpc.outputs.main_vpc
 
-  tags {
-    Tier = "${var.aws_subnet_tier}"
-    AZ = "${var.aws_subnet_az}"
+  tags = {
+    Tier = var.aws_subnet_tier
+    AZ   = var.aws_subnet_az
   }
 }
 
-data "aws_secretsmanager_secret" "sa_password" {
-  arn = "${var.aws_secrets_generic_account_password_arn}"
+# Get general account password secret for SA password
+data "aws_secretsmanager_secret" "password" {
+  arn = var.aws_secrets_generic_account_password_arn
 }
 
-data "aws_secretsmanager_secret_version" "sa_password" {
-  secret_id = "${data.aws_secretsmanager_secret.sa_password.id}"
+data "aws_secretsmanager_secret_version" "password" {
+  secret_id = data.aws_secretsmanager_secret.password.id
 }
 
 resource "aws_db_subnet_group" "mssql" {
   description = "The ${var.environment_name} RDS ${var.rds_instance_name_prefix} instance private subnet group."
-  subnet_ids  = ["${data.aws_subnet_ids.filtered_subnets.ids}"]
+  subnet_ids  = data.aws_subnet_ids.filtered_subnets.ids
 
-  tags {
-    Name        = "RDS-${var.rds_instance_name_prefix}-subnet-group-${var.environment_name}"
-    Env = "${var.environment_name}"
-    App = "${var.app_name}"
+  tags = {
+    Name      = "RDS-${var.rds_instance_name_prefix}-subnet-group-${var.environment_name}"
+    Env       = var.environment_name
     Terraform = true
   }
 }
@@ -59,37 +50,32 @@ resource "aws_db_subnet_group" "mssql" {
 resource "aws_security_group" "rds_mssql_security_group" {
   name        = "Internal-MSSQL-Traffic-To-RDS-${var.rds_instance_name_prefix}-${var.environment_name}"
   description = "Allows all VPC traffic to RDS MSSQL default port in ${var.environment_name}"
-  vpc_id      = "${data.terraform_remote_state.vpc.main_vpc}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.main_vpc
 
   ingress {
-    from_port   = 1433
-    to_port     = 1433
-    protocol    = "tcp"
-    cidr_blocks = ["${data.terraform_remote_state.vpc.main_vpc_cidr}"]
+    from_port = 1433
+    to_port   = 1433
+    protocol  = "tcp"
+    # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+    # force an interpolation expression to be interpreted as a list by wrapping it
+    # in an extra set of list brackets. That form was supported for compatibilty in
+    # v0.11, but is no longer supported in Terraform v0.12.
+    #
+    # If the expression in the following list itself returns a list, remove the
+    # brackets to avoid interpretation as a list of lists. If the expression
+    # returns a single list item then leave it as-is and remove this TODO comment.
+    cidr_blocks = [data.terraform_remote_state.vpc.outputs.main_vpc_cidr]
   }
 
-  tags {
-    Env = "${var.environment_name}"
-    App = "${var.app_name}"
+  tags = {
+    Env       = var.environment_name
     Terraform = true
-  }
-}
-
-data "aws_security_groups" "filtered_domain_controller_group" {
-  filter {
-    name   = "group-name"
-    values = ["${data.terraform_remote_state.directoryservice.directory_service_id}_controllers"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = ["${data.terraform_remote_state.vpc.main_vpc}"]
   }
 }
 
 resource "aws_db_parameter_group" "clr_enabled" {
   name   = "clr-enabled-parameters-${var.rds_instance_name_prefix}-${var.environment_name}"
-  family = "${var.mssql_engine_family}"
+  family = var.mssql_engine_family
 
   parameter {
     name  = "clr enabled"
@@ -98,8 +84,8 @@ resource "aws_db_parameter_group" "clr_enabled" {
 }
 
 resource "aws_iam_role" "iam_role_sql_backup_restore" {
-  name = "IAM_ROLE_SQL_BACKUP_RESTORE-${var.rds_instance_name_prefix}-${var.environment_name}"
-  path = "/"
+  name        = "IAM_ROLE_SQL_BACKUP_RESTORE-${var.rds_instance_name_prefix}-${var.environment_name}"
+  path        = "/"
   description = "RDS SQL Server Native Backup Without Encryption "
 
   assume_role_policy = <<EOF
@@ -116,11 +102,12 @@ resource "aws_iam_role" "iam_role_sql_backup_restore" {
   ]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy" "name" {
-  name = "IAM_POLICY_SQL_BACKUP_S3_ACCESS-${var.rds_instance_name_prefix}-${var.environment_name}"
-  role = "${aws_iam_role.iam_role_sql_backup_restore.id}"
+  name   = "IAM_POLICY_SQL_BACKUP_S3_ACCESS-${var.rds_instance_name_prefix}-${var.environment_name}"
+  role   = aws_iam_role.iam_role_sql_backup_restore.id
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -150,82 +137,48 @@ resource "aws_iam_role_policy" "name" {
     ]
 }
 EOF
+
 }
 
 resource "aws_db_option_group" "sqlexpress-native-backup-restore" {
   name                     = "sqlexpress-native-backup-restore-${var.rds_instance_name_prefix}-${var.environment_name}"
   option_group_description = "DB Option Group for backup and restore on ${var.mssql_engine_family}"
-  engine_name              = "${var.mssql_engine}"
-  major_engine_version     = "${var.engine_major_version}"
+  engine_name              = var.mssql_engine
+  major_engine_version     = var.engine_major_version
 
   option {
     option_name = "SQLSERVER_BACKUP_RESTORE"
 
     option_settings {
       name  = "IAM_ROLE_ARN"
-      value = "${aws_iam_role.iam_role_sql_backup_restore.arn}"
+      value = aws_iam_role.iam_role_sql_backup_restore.arn
     }
   }
 }
 
-/*
-#run ssm command after instance creation to register alias inside active directory
-data "template_file" "script" {
-  template = "${file("scripts/create-dns-alias.ps1")}"
-
-  vars = {
-    domain_admin_account_username = "${format("%s\\admin",var.domain_name)}"
-    aws_access_key = "${var.aws_access_key}" 
-    aws_session = "${var.aws_session}"
-    aws_secret_access_key = "${var.aws_secret_access_key}"
-    domain_admin_account_password_secret_arn = "${var.aws_secrets_domain_admin_password_arn}"
-    aws_db_instance_identifier = "${aws_db_instance.mssql.identifier}"
-    aws_db_instance_address = "${aws_db_instance.mssql.address}"
-    domain_name = "${var.domain_name}"
-  } 
-}
-
-resource "aws_ssm_association" "command" {
-  name = "AWS-RunPowerShellScript"
-  count = "${var.create_ad_domain_dns_alias}"
-
-  targets = {
-    key = "tag:Hostname"
-    values = ["${data.terraform_remote_state.sysadmin.instance_hostname}"]
-  }
-
-  parameters {
-    commands = "${data.template_file.script.rendered}"
-  }
-}
-*/
-
 resource "aws_db_instance" "mssql" {
-  depends_on = ["aws_db_subnet_group.mssql"]
-  identifier = "${var.rds_instance_name_prefix}-${var.environment_name}"
-  allocated_storage = "${var.rds_allocated_storage}"
-  license_model = "license-included"
-  storage_type = "gp2"
-  engine = "${var.mssql_engine}"
-  engine_version = "${var.engine_specific_version}"
-  instance_class = "${var.rds_instance_class}"
-  multi_az = "${var.rds_multi_az}"
-  username = "${var.mssql_admin_username}"
-  password = "${data.aws_secretsmanager_secret_version.sa_password}"
-  vpc_security_group_ids = ["${aws_security_group.rds_mssql_security_group.id}","${element(data.aws_security_groups.filtered_domain_controller_group.ids, count.index)}"]
-  db_subnet_group_name = "${aws_db_subnet_group.mssql.id}"
-  backup_retention_period = 3
-  skip_final_snapshot = "${var.rds_skip_final_snapshot}"
+  depends_on                = [aws_db_subnet_group.mssql]
+  identifier                = "${var.rds_instance_name_prefix}-${var.environment_name}"
+  allocated_storage         = var.rds_allocated_storage
+  license_model             = "license-included"
+  storage_type              = "gp2"
+  engine                    = var.mssql_engine
+  engine_version            = var.engine_specific_version
+  instance_class            = var.rds_instance_class
+  multi_az                  = var.rds_multi_az
+  username                  = var.mssql_admin_username
+  password                  = data.aws_secretsmanager_secret_version.password.secret_string
+  vpc_security_group_ids    = [aws_security_group.rds_mssql_security_group.id]
+  db_subnet_group_name      = aws_db_subnet_group.mssql.id
+  backup_retention_period   = 3
+  skip_final_snapshot       = var.rds_skip_final_snapshot
   final_snapshot_identifier = "${var.rds_instance_name_prefix}-${var.environment_name}-final-snapshot"
-  domain = "${data.terraform_remote_state.directoryservice.directory_service_id}"
-  domain_iam_role_name = "rds-directoryservice-access-role" #this is an AWS internal IAM role
-  parameter_group_name = "${aws_db_parameter_group.clr_enabled.name}"
-  option_group_name = "${aws_db_option_group.sqlexpress-native-backup-restore.name}"
+  parameter_group_name      = aws_db_parameter_group.clr_enabled.name
+  option_group_name         = aws_db_option_group.sqlexpress-native-backup-restore.name
 
-  
-  tags {
-    Env = "${var.environment_name}"
-    App = "${var.app_name}"
+  tags = {
+    Env       = var.environment_name
     Terraform = true
   }
 }
+
--- rds/variables.tf
+++ rds/variables.tf
@@ -3,8 +3,8 @@ variable "environment_name" {
   description = "Name to used to label environment deployment, for example 'dev' or 'test-lk'."
 }
 
-variable "app_name" {
-  description = "Name for labelling the deployment, for example 'sysadmin' or 'playout'"
+variable "account_id" {
+  description = "AWS account ID, used when constructing ARNs for API Gateway."
 }
 
 variable "state_bucket" {
@@ -13,22 +13,66 @@ variable "state_bucket" {
 
 variable "state_region" {
   description = "Region associated with state bucket."
-  default = "eu-west-1"
+  default     = "eu-west-1"
+}
+
+variable "global_state_bucket" {
+  description = "Name of bucket used to hold state for the global deployment."
 }
 
 variable "aws_region" {
   description = "AWS region to launch infrastructure within."
 }
 
-# Module specific variables
+variable "app_name" {
+  description = "Name to used to label application deployment, for example 'central' or 'air'."
+}
+
+variable "route53_zone_id" {
+  description = "Zone ID of the route 53 zone used to make entries (e.g. sysadmin DNS entries)"
+  default     = ""
+}
+
+variable "route53_zone_suffix" {
+  description = "Zone DNS suffix for public facing entries"
+}
+
+variable "aws_secrets_generic_account_password_arn" {
+  description = "ARN representing general password secret stored within AWS Secrets Manager"
+}
 
 variable "aws_secrets_domain_admin_password_arn" {
   description = "ARN representing domain admin password key secret stored within AWS Secrets Manager"
 }
 
-variable "domain_name" {
-  description = "Active Directory Domain Name"
+variable "aws_secrets_privatekey_arn" {
+  description = "ARN representing private PEM key secret stored within AWS Secrets Manager"
 }
+
+variable "shared_route53_zone_id" {
+  description = "Zone ID of the default shared route 53 zone used to make helper entries (e.g. sysadmin DNS entries)"
+  default     = ""
+}
+
+variable "dynamodb_table" {
+  description = "DynamoDB table used for controlling terragrunt locks"
+}
+
+variable "stage" {
+  description = "Deployment stage label, e.g. blue or green"
+  default = "blue"
+}
+
+variable "shared_route53_zone_suffix" {
+  description = "Zone DNS suffix for helper entries (e.g. sysadmin DNS entries)"
+}
+
+variable "debug_logging" {
+  description = "Debugging flag, used to increase debug logging (e.g. from within Lambda functions)"
+  default = "false"
+}
+
+# Module specific variables
 
 variable "aws_subnet_tier" {
   description = "Tier of subnet for deployment (Private / Public)"
@@ -40,54 +84,49 @@ variable "aws_subnet_az" {
 
 variable "rds_instance_class" {
   description = "Required instance class for RDS server"
-  default = "db.t2.micro"
+  default     = "db.t2.micro"
 }
 
 variable "rds_multi_az" {
   description = "Specifies if the RDS instance is multi-AZ"
-  default = "false"
+  default     = "false"
 }
 
 variable "rds_allocated_storage" {
   description = "The allocated storage in gigabytes"
-  default = "20"
+  default     = "20"
 }
 
 variable "rds_skip_final_snapshot" {
   description = "Specifies if a final snapshot should be created upon RDS destruction"
-  default = false
+  default     = false
 }
 
 variable "mssql_engine" {
   description = "AWS RDS string matching the MSSQL engine type to instance (e.g. sqlserver-ex)"
-  default = "sqlserver-ex"
+  default     = "sqlserver-ex"
 }
 
 variable "mssql_engine_family" {
   description = "AWS RDS string matching the MSSQL engine family to instance (e.g. sqlserver-ex-13.0)"
-  default = "sqlserver-ex-13.0"
+  default     = "sqlserver-ex-13.0"
 }
 
 variable "engine_specific_version" {
   description = "AWS RDS string matching the MSSQL engine specific version to instance (e.g. 13.00.5216.0.v1)"
-  default = "13.00.5216.0.v1"
+  default     = "13.00.5216.0.v1"
 }
 
 variable "engine_major_version" {
   description = "AWS RDS string matching the MSSQL engine major version to instance (e.g. 13.00)"
-  default = "13.00"
+  default     = "13.00"
 }
 
 variable "mssql_admin_username" {
   description = "Username for the administrator DB user"
-  default = "sa"
+  default     = "sa"
 }
 
 variable "rds_instance_name_prefix" {
   description = "Prefix value to use when naming created RDS instance (e.g. CINARC1)"
-}
-
-variable "aws_secrets_generic_account_password_arn" {
-  description = "ARN representing a key / value set of generic account names and passwords secrets stored within AWS Secrets Manager"
-  default = ""
 }
